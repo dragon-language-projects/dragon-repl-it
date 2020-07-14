c53a2e9a3d8ccd2b85c12d52e953b24fc89dc2e9
technowizard <16505118+austinHeisleyCook@users.noreply.github.com>
2020-07-14 16:08:38 -0700

    Update index.js

--- js./index.js
+++ js./index.js
@@ -1,4 +1,9 @@
-function showDiagol() {
+function showDragonDialog() {
 let msg "welcome to the dragon repl page.\nfeel free to browser the links";
-alert(msg);
+let chk = ""
+chk = confirm(msg);
+if(check == True {
+   alert("thank you for trying us")
+   }
+ 
 }
